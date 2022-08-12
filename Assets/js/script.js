7979c212798ac21935b962615d2fda8a5ea266b1
ZacharyFeltman <zachary.feltman@gmail.com>
2022-08-11 21:12:50 -0600

    stored answers and got score to display

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,13 +4,17 @@ var containerBox = document.querySelector(".container");
 var timerCount = document.querySelector("#timer-count");
 var timeInterval = null;
 var currentQuestionIndex = 0;
-var timeLeft = 75;
+var timeStart = 30;
+var timeLeft = 0;
 var headerDiv = document.getElementById("header");
+var answerContainer = document.getElementById("answerContainer");
 var answerBtns = document.querySelector(".answer-btns").children;
 var answerOneBtn = document.querySelector("#answer1");
 var answerTwoBtn = document.querySelector("#answer2");
 var answerThreeBtn = document.querySelector("#answer3");
 var answerFourBtn = document.querySelector("#answer4");
+var myResults = document.getElementById("results");
+var correctAnswers = 0;
 
 var questions = [
   {
@@ -60,35 +64,95 @@ $("#header").children().css("color", "blue");
 startButton.addEventListener("click", startGame);
 answerOneBtn.addEventListener("click", function () {
 	checkAnswer(0);
+	if (currentQuestionIndex < shuffledQuestions.length-1) {
+		currentQuestionIndex++;
+		showNextQuestion();
+	} else {
+		// done w/ quiz
+		endGame();
+	}
+	//console.log(shuffledQuestions);
 });
 answerTwoBtn.addEventListener("click", function () {
 	checkAnswer(1);
+	if (currentQuestionIndex < shuffledQuestions.length-1) {
+		currentQuestionIndex++;
+		showNextQuestion();
+	} else {
+		// done w/ quiz
+		endGame();
+	}
+	//console.log(shuffledQuestions);
 });
 answerThreeBtn.addEventListener("click", function () {
 	checkAnswer(2);
+	if (currentQuestionIndex < shuffledQuestions.length-1) {
+		currentQuestionIndex++;
+		showNextQuestion();
+	} else {
+		// done w/ quiz
+		endGame();
+	}
+	//console.log(shuffledQuestions);
 });
 answerFourBtn.addEventListener("click", function () {
 	checkAnswer(3);
+	if (currentQuestionIndex < shuffledQuestions.length-1) {
+		currentQuestionIndex++;
+		showNextQuestion();
+	} else {
+		// done w/ quiz
+		endGame();
+	}
+	//console.log(shuffledQuestions);
 });
 
 function checkAnswer(index) {
 	let currentQuestion = shuffledQuestions[currentQuestionIndex];
 	console.log(currentQuestion);
 	let currentAnswer = currentQuestion.answers[index];
-	if (currentAnswer.correct) {
-		alert("correct") 
+	if (currentAnswer.correct) { 
+		correctAnswers++;
 	} else {
-		alert("incorrect")
+		timeLeft = timeLeft-5;
+		//subtract time
 	}
+}
+
+function endGame() {
+	timeLeft = 0;
+	clearInterval(timeInterval);
+	questionContainer.style.display = "none";
+	answerContainer.style.display = "none";	
+	startButton.style.display = "block";
+
+	var myPrompt = prompt("You got " + correctAnswers + " out of 4 correct. Please enter your initials:");
+	if (myPrompt != null) {
+		localStorage.setItem("initials", myPrompt);
+		localStorage.setItem("score", correctAnswers);
+	}
+
+	myResults.style.display = "block";
+	myResults.textContent = "Previous score: " + localStorage.getItem("score") + "/4 for " + localStorage.getItem("initials");
+
 }
 
 
 
 
 function startGame() {
+	timeLeft = timeStart;
 	shuffleQuestions();
 	currentQuestionIndex = 0;
+	startButton.style.display = "none";
+	questionContainer.style.display = "block";
+	answerContainer.style.display = "block";	
+
 	showNextQuestion();
+	countDownTimer();
+	timeInterval = setInterval(function() {
+		countDownTimer();
+	}, 1000);
 	
 }
 
@@ -103,13 +167,20 @@ function shuffleQuestions() {
 }
 
 //setting allowed time for quiz
-function countDownTimer() {
-	timeLeft--;
-  	timerCount.textContent = timeLeft;
-  	if (timeLeft === 0) {
-		clearInterval(timeInterval);
-  	}
+function countDownTimer(seconds) {
+	
+  	timerCount.textContent = "Time remaining: " + timeLeft;
+
+  	if (timeLeft <= 0) {
+		timerCount.textContent = "No time left";
+		// clearInterval(timeInterval);
+		endGame();
+  	} else {
+		timeLeft--;
+	}
 }
+
+
 
 function showNextQuestion() {
 	let currentQuestion = shuffledQuestions[currentQuestionIndex];
@@ -118,7 +189,6 @@ function showNextQuestion() {
 	answerTwoBtn.textContent = currentQuestion.answers[1].text;
 	answerThreeBtn.textContent = currentQuestion.answers[2].text;
 	answerFourBtn.textContent = currentQuestion.answers[3].text;
-
 
 }
 
